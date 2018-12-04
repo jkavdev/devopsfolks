ba943a36c97a77cfd2ac1fceadb47b3598fcaabc
jkavdev <jhonatankolen@gmail.com>
2018-12-04 00:02:44 -0200

    Adicionando internacionalizacao - i18n

--- devopsbuddy/src/test/java/br/com/jkavdev/devopsbuddy/DevopsbuddyApplicationTests.java
+++ devopsbuddy/src/test/java/br/com/jkavdev/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -1,7 +1,10 @@
 package br.com.jkavdev.devopsbuddy;
 
+import br.com.jkavdev.devopsbuddy.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DevopsbuddyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private I18NService i18NService;
+
+    @Test
+    public void testMessageByLocaleService() throws Exception {
+        String expectedResult = "Bootstrap starter template";
+        String messageId = "index.main.callout";
+        String actual = i18NService.getMessage(messageId);
+        Assert.assertEquals("As String atuais e a esperadas nao batem", expectedResult, actual);
+    }
 
 }
