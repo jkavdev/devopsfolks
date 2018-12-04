68485e352219c2809453b93ed528eb5fa454ccdb
jkavdev <jhonatankolen@gmail.com>
2018-12-04 00:26:41 -0200

    Adicionando log, e configurando um live-template para o intellij

--- devopsbuddy/src/main/java/br/com/jkavdev/devopsbuddy/web/i18n/I18NService.java
+++ devopsbuddy/src/main/java/br/com/jkavdev/devopsbuddy/web/i18n/I18NService.java
@@ -1,5 +1,7 @@
 package br.com.jkavdev.devopsbuddy.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,10 +12,16 @@ import java.util.Locale;
 @Service
 public class I18NService {
 
+    /**
+     * The Application Logger
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+
     @Autowired
     private MessageSource messageSource;
 
     public String getMessage(String messageId) {
+        LOG.info("Retornando texto i18n para a messageId {}", messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId, locale);
     }
