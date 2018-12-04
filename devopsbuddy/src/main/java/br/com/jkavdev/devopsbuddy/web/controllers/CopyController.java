ce55863bab2268f31963fe963aed7b75ddf38b16
jkavdev <jhonatankolen@gmail.com>
2018-12-04 01:50:27 -0200

    Adicionando pagina about, junto com estilo e imagens

---
+++ devopsbuddy/src/main/java/br/com/jkavdev/devopsbuddy/web/controllers/CopyController.java
@@ -0,0 +1,14 @@
+package br.com.jkavdev.devopsbuddy.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CopyController {
+
+	@RequestMapping("/about")
+	public String about() {
+		return "copy/about";
+	}
+
+}
